599f979b6d3fd5b2479517f723d5bad2640af626
ikprx <ikprx@github.com>
2018-09-20 14:36:03 +0200

    Resource Manager implemented

---
+++ src/ResourceManager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <map>
+#include <GL/glew.h>
+#include <string>
+#include "Shader.h"
+
+
+class ResourceManager{
+public:
+    static std::map<std::string, Shader> shaders;
+    static Shader loadShader(std::string name, const GLchar * vShaderFile, const GLchar * fShaderFile);
+    static Shader getShader(std::string name);
+
+    static void clear();
+private:
+    ResourceManager() {}
+    static Shader loadShaderFromFile(const GLchar * vShaderFile, const GLchar * fShaderFile);
+    static std::string loadFile(const GLchar * path);
+};
